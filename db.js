7134eee62510917b4848bbf76bb3000353c86295
Hansraj Meena <hansrajmeena@Hansrajs-MacBook-Air.local>
2026-06-20 10:03:26 +0530

    Use persistent disk for SQLite

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database(
-    "./database.db",
+    "/opt/render/project/src/data/database.db",
     err => {
 
         if(err){
